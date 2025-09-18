2c80cc877bceb20e69b9463975b4cdcc1f47be07
Salvador Depons <50385063+SalvaDepons18@users.noreply.github.com>
2025-09-18 14:30:40 -0300

    add functions to InstitutionRegistry

--- src/InstitutionRegistry.sol
+++ src/InstitutionRegistry.sol
@@ -14,5 +14,29 @@ contract InstitutionRegistry is AccessControl {
    
     bytes32 public constant ISSUER_ROLE = keccak256("ISSUER_ROLE");
 
+    // Custom errors cheaper than strings (GAS)
+    error ZeroAddress();
+    error AlreadyIssuer();
+    error NotAnIssuer();
+
+    constructor(address initialAdmin){
+        if(initialAdmin == address(0)) revert ZeroAddress();
+        _grantRole(DEFAULT_ADMIN_ROLE, initialAdmin);
+    }
+
+    function grantIssuer(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (account == address(0)) revert ZeroAddress();
+        if (hasRole(ISSUER_ROLE, account)) revert AlreadyIssuer();
+        _grantRole(ISSUER_ROLE, account);
+    }
+
+    function revokeIssuer(address account) external onlyRole(DEFAULT_ADMIN_ROLE){
+        if(!hasRole(ISSUER_ROLE,account)) revert NotAnIssuer();
+        _revokeRole(ISSUER_ROLE,account);
+    }
+
+    function isIssuer(address account) external view returns (bool){
+        return hasRole(ISSUER_ROLE,account);
+    }
 
 }
